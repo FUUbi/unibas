178626954fc660f0190df496a60cca66e7688ca8
Fabrizio <fabrizio.parrillo@fhnw.ch>
2018-03-02 14:18:14 +0100

    ex 2.a)

--- AaD/blatt01/src/test/java/SortTests.java
+++ AaD/blatt01/src/test/java/SortTests.java
@@ -35,4 +35,21 @@ public class SortTests  {
     Assert.assertArrayEquals(arrayToSort, resultingArray);
   }
 
+  @Test
+  public void mergeSortShouldCorrectlySortEmptyIntArray() {
+    int[] emptyArray = new int[0];
+    int[] resultingArray = new int[0];
+    MergeSort.sort(emptyArray);
+    Assert.assertArrayEquals(emptyArray, resultingArray);
+  }
+
+  @Test
+  public void mergeSortShouldCorrectlySortNonemptyIntArray() {
+    int[] arrayToSort = new int[] {1, 4, 2, 5, 4};
+    int[] resultingArray = new int[] {1, 2, 4, 4, 5};
+    MergeSort.sort(arrayToSort);
+    Assert.assertArrayEquals(arrayToSort, resultingArray);
+  }
+
+
 }
